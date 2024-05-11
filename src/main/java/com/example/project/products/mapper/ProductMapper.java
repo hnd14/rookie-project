5e6ee0384e4da353e32d26a651931ca56d2e8519
hnd14 <hnd14@case.edu>
2024-05-11 19:40:25 +0700

    Add product mapper

---
+++ src/main/java/com/example/project/products/mapper/ProductMapper.java
@@ -0,0 +1,13 @@
+package com.example.project.products.mapper;
+
+import org.mapstruct.Mapper;
+
+import com.example.project.products.dto.Requests.PostNewProductDto;
+import com.example.project.products.dto.Responses.ProductCustomerDto;
+import com.example.project.products.entities.Product;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+    ProductCustomerDto toCustomerDto(Product product);
+    Product toProductForNew(PostNewProductDto dto);
+}
