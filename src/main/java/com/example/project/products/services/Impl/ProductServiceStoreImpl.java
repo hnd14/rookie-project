25358b98bcf05c967a79629855ea41df085b8810
hnd14 <hnd14@case.edu>
2024-05-11 20:10:03 +0700

    find product by id

--- src/main/java/com/example/project/products/services/Impl/ProductServiceStoreImpl.java
+++ src/main/java/com/example/project/products/services/Impl/ProductServiceStoreImpl.java
@@ -1,8 +1,16 @@
 package com.example.project.products.services.Impl;
 
+import com.example.project.products.dto.Responses.ProductCustomerDto;
+import com.example.project.products.exceptions.ProductNotFoundException;
+import com.example.project.products.mapper.ProductMapper;
 import com.example.project.products.repositories.ProductRepository;
 import com.example.project.products.services.ProductServiceStore;
 
 public class ProductServiceStoreImpl implements ProductServiceStore{
     private ProductRepository repo;
+    private ProductMapper mapper;
+
+    public ProductCustomerDto getProductById(Long id){
+        return repo.findById(id).map(mapper::toCustomerDto).orElseThrow(ProductNotFoundException::new);
+    }
 }
