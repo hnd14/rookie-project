c1f8562bbcaf961adedfd1e8fe3fb3b0eb64552f
hnd14 <hnd14@case.edu>
2024-05-11 19:11:15 +0700

    add back store interface

---
+++ src/main/java/com/example/project/products/services/ProductServiceBackStore.java
@@ -0,0 +1,5 @@
+package com.example.project.products.services;
+
+public interface ProductServiceBackStore {
+
+}
--- src/main/java/com/example/project/products/services/ProductServiceStore.java
+++ src/main/java/com/example/project/products/services/ProductServiceStore.java
@@ -3,6 +3,6 @@ package com.example.project.products.services;
 import org.springframework.stereotype.Service;
 
 @Service
-public interface ProductService {
+public interface ProductServiceStore {
     
 }
