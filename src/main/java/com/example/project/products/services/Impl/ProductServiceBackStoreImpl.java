9c7fc3cbd68b0eb428430767e17f65aa972ccd21
hnd14 <hnd14@case.edu>
2024-05-13 13:43:54 +0700

    Add new services

--- src/main/java/com/example/project/products/services/Impl/ProductServiceBackStoreImpl.java
+++ src/main/java/com/example/project/products/services/Impl/ProductServiceBackStoreImpl.java
@@ -1,20 +1,40 @@
 package com.example.project.products.services.Impl;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import com.example.project.products.dto.Requests.PostNewProductDto;
+import com.example.project.products.dto.Requests.ProductSearchDto;
 import com.example.project.products.dto.Responses.ProductSellerDto;
 import com.example.project.products.entities.Product;
+import com.example.project.products.exceptions.ProductNotFoundException;
 import com.example.project.products.mapper.ProductMapper;
 import com.example.project.products.repositories.ProductRepository;
 import com.example.project.products.services.ProductServiceBackStore;
 
+@Service
 public class ProductServiceBackStoreImpl implements ProductServiceBackStore {
+    @Autowired
     ProductRepository repo;
+    @Autowired
     ProductMapper mapper;
+    
     @Override
     public ProductSellerDto createNewProduct(PostNewProductDto dto) {
         Product newProduct = mapper.toNewProduct(dto);
         repo.save(newProduct);
         return mapper.toStaffDto(newProduct);
+    }
+    @Override
+    public ProductSellerDto getProductById(Long id) {
+        return repo.findById(id).map(mapper::toStaffDto).orElseThrow(ProductNotFoundException::new);
+    }
+    @Override
+    public List<ProductSellerDto> findProductWithFilter(ProductSearchDto dto) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'findProductWithFilter'");
     }
 
 }
--- src/main/java/com/example/project/products/services/ProductServiceBackStore.java
+++ src/main/java/com/example/project/products/services/ProductServiceBackStore.java
@@ -1,11 +1,17 @@
 package com.example.project.products.services;
 
+import java.util.List;
+
 import org.springframework.stereotype.Service;
 
 import com.example.project.products.dto.Requests.PostNewProductDto;
+import com.example.project.products.dto.Requests.ProductSearchDto;
 import com.example.project.products.dto.Responses.ProductSellerDto;
 
 @Service
 public interface ProductServiceBackStore {
     ProductSellerDto createNewProduct(PostNewProductDto dto);
+    ProductSellerDto getProductById(Long id);
+    List<ProductSellerDto> findProductWithFilter(ProductSearchDto dto);
 }
+
