1df5c41bf451f6ba28a5967ea2ef6f8b34614fd1
hnd14 <hnd14@case.edu>
2024-05-13 16:08:04 +0700

    send more info to customer side

--- src/main/java/com/example/project/products/dto/Responses/ProductCustomerDto.java
+++ src/main/java/com/example/project/products/dto/Responses/ProductCustomerDto.java
@@ -2,4 +2,5 @@ package com.example.project.products.dto.Responses;
 
 public record ProductCustomerDto(Long id,
                                  String name,
-                                 String desc) {}
+                                 String desc,
+                                 Double salePrice) {}
