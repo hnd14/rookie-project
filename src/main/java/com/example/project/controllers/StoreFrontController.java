599460975e5459490cfb13b4e24e7d109c949b47
hnd14 <hnd14@case.edu>
2024-05-13 16:02:05 +0700

    Add store front  controller

--- src/main/java/com/example/project/controllers/StoreFrontController.java
+++ src/main/java/com/example/project/controllers/StoreFrontController.java
@@ -1,10 +1,32 @@
 package com.example.project.controllers;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.example.project.products.dto.Requests.ProductSearchDto;
+import com.example.project.products.dto.Responses.ProductCustomerDto;
+import com.example.project.products.services.ProductServiceStore;
 
 @RestController
 @RequestMapping("/store")
 public class StoreFrontController {
+    @Autowired
+    ProductServiceStore productService;
     
+    @GetMapping("/products/{id}")
+    ProductCustomerDto getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
+
+    @GetMapping("/products")
+    @ResponseBody
+    List<ProductCustomerDto> findProductsWithFilter(ProductSearchDto dto){
+        return productService.findProductWithFilter(dto);
+    }
 }
