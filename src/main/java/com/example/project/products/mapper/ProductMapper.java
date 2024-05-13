63a3a203233baea47d000cf73c33a86a708bdb76
hnd14 <hnd14@case.edu>
2024-05-13 13:04:01 +0700

    add new mapper

--- src/main/java/com/example/project/products/mapper/ProductMapper.java
+++ src/main/java/com/example/project/products/mapper/ProductMapper.java
@@ -4,10 +4,12 @@ import org.mapstruct.Mapper;
 
 import com.example.project.products.dto.Requests.PostNewProductDto;
 import com.example.project.products.dto.Responses.ProductCustomerDto;
+import com.example.project.products.dto.Responses.ProductSellerDto;
 import com.example.project.products.entities.Product;
 
 @Mapper(componentModel = "spring")
 public interface ProductMapper {
     ProductCustomerDto toCustomerDto(Product product);
-    Product toProductForNew(PostNewProductDto dto);
+    Product toNewProduct(PostNewProductDto dto);
+    ProductSellerDto toStaffDto(Product product);
 }
