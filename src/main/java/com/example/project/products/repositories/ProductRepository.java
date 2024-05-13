34e52b7c72346e4c2ca5ab974eca08d9a916195c
hnd14 <hnd14@case.edu>
2024-05-13 15:55:36 +0700

    Refactor code

--- src/main/java/com/example/project/products/repositories/ProductRepository.java
+++ src/main/java/com/example/project/products/repositories/ProductRepository.java
@@ -1,11 +1,12 @@
 package com.example.project.products.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
 
 import com.example.project.products.entities.Product;
 
 @Repository
-public interface ProductRepository extends JpaRepository<Product,Long> {
+public interface ProductRepository extends JpaRepository<Product,Long>, JpaSpecificationExecutor<Product> {
 
 }
--- src/main/java/com/example/project/products/services/Impl/ProductServiceBackStoreImpl.java
+++ src/main/java/com/example/project/products/services/Impl/ProductServiceBackStoreImpl.java
@@ -4,7 +4,6 @@ import java.util.List;
 import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
 import com.example.project.products.dto.Requests.PostNewProductDto;
