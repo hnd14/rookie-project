3048624d3ac090ac920685bc3f0a3613141eef64
hnd14 <hnd14@case.edu>
2024-05-13 13:43:28 +0700

    Turn mapper to component

--- src/main/java/com/example/project/products/mapper/ProductMapper.java
+++ src/main/java/com/example/project/products/mapper/ProductMapper.java
@@ -1,6 +1,7 @@
 package com.example.project.products.mapper;
 
 import org.mapstruct.Mapper;
+import org.springframework.stereotype.Component;
 
 import com.example.project.products.dto.Requests.PostNewProductDto;
 import com.example.project.products.dto.Responses.ProductCustomerDto;
@@ -8,6 +9,7 @@ import com.example.project.products.dto.Responses.ProductSellerDto;
 import com.example.project.products.entities.Product;
 
 @Mapper(componentModel = "spring")
+@Component
 public interface ProductMapper {
     ProductCustomerDto toCustomerDto(Product product);
     Product toNewProduct(PostNewProductDto dto);
