f2c114113dcb0137cbf1ed6bf6bebfe8b92be77c
hnd14 <hnd14@case.edu>
2024-05-13 13:44:46 +0700

    Add new api

--- src/main/java/com/example/project/controllers/StoreBackController.java
+++ src/main/java/com/example/project/controllers/StoreBackController.java
@@ -1,10 +1,33 @@
 package com.example.project.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.project.products.dto.Requests.PostNewProductDto;
+import com.example.project.products.dto.Responses.ProductSellerDto;
+import com.example.project.products.services.ProductServiceBackStore;
+
+import lombok.AllArgsConstructor;
+
 @RestController
 @RequestMapping("/store-back")
+@AllArgsConstructor
 public class StoreBackController {
+    private final ProductServiceBackStore productService; 
 
+    @PostMapping("/products")
+    public ProductSellerDto createNewProduct(@RequestBody PostNewProductDto dto){
+        System.out.println(dto.salePrice());
+        return productService.createNewProduct(dto);
+    }
+
+    @GetMapping("/products/{id}")
+    public ProductSellerDto getProductById(@PathVariable Long id){
+        return productService.getProductById(id);
+    }
 }
