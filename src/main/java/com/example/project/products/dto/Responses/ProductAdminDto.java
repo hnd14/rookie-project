acb1f63af67ec49bca679c3ea9f400acb880af09
hnd14 <hnd14@case.edu>
2024-05-14 09:11:50 +0700

    rename dto

---
+++ src/main/java/com/example/project/products/dto/Responses/ProductAdminDto.java
@@ -0,0 +1,5 @@
+package com.example.project.products.dto.Responses;
+
+public record ProductAdminDto(Long id, String name, String desc, Double salePrice, Long stock) {
+
+}
--- src/main/java/com/example/project/products/dto/Responses/ProductSellerDto.java
+++
@@ -1,5 +0,0 @@
-package com.example.project.products.dto.Responses;
-
-public record ProductSellerDto(Long id, String name, String desc, Double salePrice, Long stock) {
-
-}
