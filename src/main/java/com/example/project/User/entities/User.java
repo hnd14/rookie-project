f0d9851ba37e27d6d2e9b99b0b695d264da4b220
hnd14 <hnd14@case.edu>
2024-05-15 10:29:23 +0700

    init entities for users

---
+++ src/main/java/com/example/project/User/entities/User.java
@@ -0,0 +1,30 @@
+package com.example.project.User.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embedded;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+import jakarta.persistence.SecondaryTable;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+@SecondaryTable(name = "users_details", pkJoinColumns = @PrimaryKeyJoinColumn(name = "users_id"))
+public class User {
+    @Column
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    private Long id;
+
+    @Column
+    private String username;
+    
+    @Column
+    private String password;
+
+    @Embedded
+    private UserDetails userDetails;
+}
---
+++ src/main/java/com/example/project/User/entities/UserDetails.java
@@ -0,0 +1,16 @@
+package com.example.project.User.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+
+@Embeddable
+public class UserDetails {
+    @Column(name = "phone_number", table = "user_details")
+    String phoneNumber;
+
+    @Column(name = "billing_address", table = "user_details")
+    String billingAddress;
+
+    @Column(name = "shipping_address", table = "user_details")
+    String shippingAddress;
+}
