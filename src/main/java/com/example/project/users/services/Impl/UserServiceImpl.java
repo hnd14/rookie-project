4073dba4ad61b8ae4d2ee8ac47ea5222ce19e46d
hnd14 <hnd14@case.edu>
2024-05-16 16:39:53 +0700

    Add annotations for product service

--- src/main/java/com/example/project/users/services/Impl/UserServiceImpl.java
+++ src/main/java/com/example/project/users/services/Impl/UserServiceImpl.java
@@ -1,7 +1,13 @@
 package com.example.project.users.services.Impl;
 
+import org.springframework.stereotype.Service;
+
 import com.example.project.users.services.UserService;
 
+import jakarta.transaction.Transactional;
+
+@Service
+@Transactional
 public class UserServiceImpl implements UserService {
 
 }
