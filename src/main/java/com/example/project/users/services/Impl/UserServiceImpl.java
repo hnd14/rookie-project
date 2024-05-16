295af98c08c7ae54572535284cec2bbeacccf23d
hnd14 <hnd14@case.edu>
2024-05-16 14:14:20 +0700

    Init user services

---
+++ src/main/java/com/example/project/users/services/Impl/UserServiceImpl.java
@@ -0,0 +1,7 @@
+package com.example.project.users.services.Impl;
+
+import com.example.project.users.services.UserService;
+
+public class UserServiceImpl implements UserService {
+
+}
---
+++ src/main/java/com/example/project/users/services/UserService.java
@@ -0,0 +1,5 @@
+package com.example.project.users.services;
+
+public interface UserService {
+
+}
