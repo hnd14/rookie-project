402978fbdb620e8da13cd0dde54c3d983cd72c2b
hnd14 <hnd14@case.edu>
2024-05-16 16:47:48 +0700

    refactor

--- src/main/java/com/example/project/products/entities/ProductCategory.java
+++ src/main/java/com/example/project/products/entities/ProductCategory.java
@@ -1,6 +1,5 @@
 package com.example.project.products.entities;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
