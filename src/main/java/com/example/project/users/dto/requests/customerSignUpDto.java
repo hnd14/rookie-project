38ec8dd8b9bf6e2e117a1e11e742692b37cc2d52
hnd14 <hnd14@case.edu>
2024-05-17 17:33:46 +0700

    Rename dto

--- src/main/java/com/example/project/users/dto/requests/customerSignUpDto.java
+++ src/main/java/com/example/project/users/dto/requests/customerSignUpDto.java
@@ -4,6 +4,8 @@ import com.example.project.users.entities.UserDetails;
 
 import jakarta.validation.constraints.NotBlank;
 
-public record customerSignUpDto(@NotBlank String username, @NotBlank String password, UserDetails userDetails) {
+public record CustomerSignUpDto(@NotBlank String username, @NotBlank String password, 
+@NotBlank String email, 
+UserDetails userDetails) {
 
 }
