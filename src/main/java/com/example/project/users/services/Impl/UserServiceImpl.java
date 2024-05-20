767c1818c61f55ce0847224fd8701ea7f2e225c3
hnd14 <hnd14@case.edu>
2024-05-20 17:21:28 +0700

    User Crude services

--- src/main/java/com/example/project/users/services/Impl/UserServiceImpl.java
+++ src/main/java/com/example/project/users/services/Impl/UserServiceImpl.java
@@ -1,20 +1,24 @@
 package com.example.project.users.services.Impl;
 
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.project.users.dto.requests.CreateNewAdminDto;
 import com.example.project.users.dto.requests.CustomerSignUpDto;
+import com.example.project.users.dto.requests.UpdateUserInfoDto;
 import com.example.project.users.dto.responses.UserReturnDto;
 import com.example.project.users.entities.User;
 import com.example.project.users.repositories.UserRepository;
 import com.example.project.users.services.UserService;
+import com.example.project.util.entities.NotFoundException;
 import com.example.project.util.entities.PasswordEncoder;
 
-import jakarta.transaction.Transactional;
+
 
 @Service
-@Transactional
+@Transactional(readOnly=true)
 public class UserServiceImpl implements UserService {
     @Autowired
     private PasswordEncoder passwordEncoder;
@@ -33,11 +37,29 @@ public class UserServiceImpl implements UserService {
 
     @Transactional
     public UserReturnDto signUp(CustomerSignUpDto dto){
-        User newAdmin = new User();
-        newAdmin.setUsername(dto.username());
-        newAdmin.setEmail(dto.email());
-        newAdmin.setPassword(passwordEncoder.encode(dto.password()));
-        repository.save(newAdmin);
-        return new UserReturnDto(newAdmin.getUsername(), newAdmin.getEmail());
+        User newUser = new User();
+        newUser.setUsername(dto.username());
+        newUser.setEmail(dto.email());
+        newUser.setPassword(passwordEncoder.encode(dto.password()));
+        repository.save(newUser);
+        return new UserReturnDto(newUser.getUsername(), newUser.getEmail());
     }
+
+    public UserReturnDto getUserById(Long id){
+        User res = repository.findById(id).orElseThrow(NotFoundException::new);
+        return new UserReturnDto(res.getUsername(), res.getEmail());
+    }
+
+    public UserReturnDto updateUserInfo(Long id,UpdateUserInfoDto dto){
+        User userToUpdate = repository.findById(id).orElseThrow(NotFoundException::new);
+        userToUpdate.setPassword(passwordEncoder.encode(dto.rawPassword()));
+        userToUpdate.setUserDetails(dto.details());
+        repository.save(userToUpdate);
+        return new UserReturnDto(userToUpdate.getUsername(), userToUpdate.getEmail());
+    }
+
+    public void deleteUser(Long id){
+        repository.deleteById(id);
+    }
+    
 }
