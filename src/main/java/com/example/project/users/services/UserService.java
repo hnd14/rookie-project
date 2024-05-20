f34384db6ee3ee3e6bbc1350ee7fa8b1e036c6dc
hnd14 <hnd14@case.edu>
2024-05-20 17:40:33 +0700

    Add interface for user service

--- src/main/java/com/example/project/users/services/UserService.java
+++ src/main/java/com/example/project/users/services/UserService.java
@@ -1,5 +1,21 @@
 package com.example.project.users.services;
 
+
+import com.example.project.users.dto.requests.CreateNewAdminDto;
+import com.example.project.users.dto.requests.CustomerSignUpDto;
+import com.example.project.users.dto.requests.UpdateUserInfoDto;
+import com.example.project.users.dto.responses.UserReturnDto;
+
 public interface UserService {
+
+    UserReturnDto createNewAdminUser(CreateNewAdminDto dto);
+
+    UserReturnDto signUp(CustomerSignUpDto dto);
+
+    UserReturnDto getUserById(Long id);
+
+    UserReturnDto updateUserInfo(Long id, UpdateUserInfoDto dto);
+
+    void deleteUser(Long id);
 
 }
