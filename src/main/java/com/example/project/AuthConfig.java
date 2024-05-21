bda4a7408231189d08b930f50e5a386020b10632
hnd14 <hnd14@case.edu>
2024-05-21 17:53:57 +0700

    Adding auth config for development

---
+++ src/main/java/com/example/project/AuthConfig.java
@@ -0,0 +1,33 @@
+package com.example.project;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.example.project.jwt.JwtAuthenticationFilter;
+import com.example.project.jwt.TokenProvider;
+
+@Configuration
+public class AuthConfig {
+    @Bean
+    JwtAuthenticationFilter filter(TokenProvider tokenProvider, UserDetailsService userService){
+        return new JwtAuthenticationFilter(tokenProvider, userService);
+    }
+
+    @Bean
+    SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity, JwtAuthenticationFilter authFilter) throws Exception{
+        return httpSecurity.csrf(AbstractHttpConfigurer::disable)
+        .authorizeHttpRequests(authorize -> authorize.anyRequest().permitAll())
+        .build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder () {
+        return new BCryptPasswordEncoder();
+    }
+}
