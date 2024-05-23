ea49975e7f7ba5c0c81cd4e927b0907051c1a09f
hnd14 <hnd14@case.edu>
2024-05-23 14:12:21 +0700

    Implement rating services

--- src/main/java/com/example/project/ratings/repositories/RatingRepository.java
+++ src/main/java/com/example/project/ratings/repositories/RatingRepository.java
@@ -1,11 +1,13 @@
 package com.example.project.ratings.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import com.example.project.ratings.entities.Rating;
 
 @Repository
 public interface RatingRepository extends JpaRepository<Rating, Long>{
-   
+    @Query(value = "SELECT AVG() FROM Rating r WHERE r.product.id = ?1")
+    Double findAverageRatingForProduct(Long productId);
 }
---
+++ src/main/java/com/example/project/ratings/services/impl/RatingServiceImpl.java
@@ -0,0 +1,47 @@
+package com.example.project.ratings.services.impl;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AnonymousAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.project.products.exceptions.ProductNotFoundException;
+import com.example.project.products.repositories.ProductRepository;
+import com.example.project.ratings.dto.requests.GetAverageRatingDto;
+import com.example.project.ratings.dto.requests.PostNewRatingDto;
+import com.example.project.ratings.dto.responses.AverageRatingDto;
+import com.example.project.ratings.dto.responses.NewRatingPostedDto;
+import com.example.project.ratings.entities.Rating;
+import com.example.project.ratings.repositories.RatingRepository;
+import com.example.project.users.entities.User;
+import com.example.project.users.repositories.UserRepository;
+
+public class RatingServiceImpl {
+    @Autowired
+    private RatingRepository ratingRepository;
+    @Autowired
+    private ProductRepository productRepository;
+    @Autowired
+    private UserRepository userRepository;
+
+    public NewRatingPostedDto postNewRating(PostNewRatingDto dto){
+        var authentication = SecurityContextHolder.getContext().getAuthentication();
+        if (authentication == null 
+            || !authentication.isAuthenticated()
+            || authentication instanceof AnonymousAuthenticationToken){
+            throw new SecurityException("You need to log in to leave a rating");
+        }
+        var username = authentication.getPrincipal().toString();
+        Rating newRating = new Rating();
+        newRating.setScores(dto.scores());
+        newRating.setProduct(productRepository.findById(dto.productId()).orElseThrow(ProductNotFoundException::new));
+        User user =userRepository.findOneByUsername(username).orElseThrow(()-> new UsernameNotFoundException("Username not found"));
+        newRating.setUser(user);
+        ratingRepository.save(newRating);
+        return new NewRatingPostedDto(newRating.getId());
+    }
+
+    public AverageRatingDto getAvgRating(GetAverageRatingDto dto){
+        return new AverageRatingDto(ratingRepository.findAverageRatingForProduct(dto.productId()));
+    }
+}
