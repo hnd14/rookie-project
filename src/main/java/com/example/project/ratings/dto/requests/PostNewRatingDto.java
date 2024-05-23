8d43411fd1341ae6e1af88930d90c56216fc1463
hnd14 <hnd14@case.edu>
2024-05-23 14:28:12 +0700

    Update rating dto

--- src/main/java/com/example/project/ratings/dto/requests/PostNewRatingDto.java
+++ src/main/java/com/example/project/ratings/dto/requests/PostNewRatingDto.java
@@ -4,8 +4,7 @@ import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
-public record PostNewRatingDto(@NotBlank Long userId,
-                                @NotBlank Long productId,
+public record PostNewRatingDto(@NotBlank Long productId,
                                 @NotBlank @Min(value = 1) @Max(value=5) Integer scores) {
 
 }
