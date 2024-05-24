37bc818317c16cc5aea2a30c2677280b9f38b213
hnd14 <hnd14@case.edu>
2024-05-24 10:09:21 +0700

    add v1 mapping

--- src/main/java/com/example/project/controllers/V1Rest.java
+++ src/main/java/com/example/project/controllers/V1Rest.java
@@ -2,7 +2,7 @@ package com.example.project.controllers;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 
-@RequestMapping
+@RequestMapping("/v1")
 public abstract class V1Rest {
 
 }
