4925823b8abc62d4d98322829601130c22ac66e2
hnd14 <hnd14@case.edu>
2024-05-29 16:17:36 +0700

    Details product dto

---
+++ src/main/java/com/example/project/products/dto/Responses/ProductDetailsAdminDto.java
@@ -0,0 +1,28 @@
+package com.example.project.products.dto.Responses;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ProductDetailsAdminDto {    
+    private Long id;
+    private String name;
+    private String desc; 
+    private Double salePrice; 
+    private Long stock;
+    private Boolean isFeatured; 
+    private String createdBy; 
+    private String updatedBy;
+    private LocalDateTime createdTime;
+    private LocalDateTime updatedTime;
+    private List<SimpleCategoryDto> categoriesInfo; 
+
+}
