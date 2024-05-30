0c779d8357b88ffbad8a3ff7c54ed072abeb4e98
hnd14 <hnd14@case.edu>
2024-05-30 15:12:19 +0700

    Rename dto

--- src/main/java/com/example/project/util/entities/PagingDto.java
+++ src/main/java/com/example/project/util/entities/PagingDto.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 import jakarta.persistence.MappedSuperclass;
 
 @MappedSuperclass
-public record PagedDto(Optional<String> sortBy,
+public record PagingDto(Optional<String> sortBy,
   Optional<String> direction, 
   Optional<Integer> pageSize,
   Optional<Integer> pageNumber) {
