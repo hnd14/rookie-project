fed0dc112a333ffa32f2f84be35c15cae4ccc143
hnd14 <hnd14@case.edu>
2024-06-03 12:39:29 +0700

    Add more product service tests

--- src/test/java/com/example/project/products/services/Impl/ProductServiceImplTest.java
+++ src/test/java/com/example/project/products/services/Impl/ProductServiceImplTest.java
@@ -3,6 +3,8 @@ package com.example.project.products.services.Impl;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
+import java.util.Optional;
+
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -14,6 +16,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
+import com.example.project.products.dto.Requests.ProductSearchDto;
 import com.example.project.products.entities.Product;
 import com.example.project.products.exceptions.ProductNotFoundException;
 import com.example.project.products.repositories.ProductRepository;
@@ -28,29 +31,143 @@ public class ProductServiceImplTest {
     @Autowired
     private ProductServiceImpl service;
     private Product productInDB;
-    
+    private final ProductSearchDto defaultSerch = new ProductSearchDto(Optional.of(""),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.empty(),
+            Optional.of("name"),
+            Optional.of("ASC"),
+            Optional.of(10),
+            Optional.of(1),
+            Optional.empty());
+
     @BeforeAll
-    void setUp(){
+    void setUp() {
         repository.deleteAll();
     }
 
     @BeforeEach
-    void setUpDB(){
+    void setUpDB() {
         productInDB = new Product();
         productInDB.setName("product");
         productInDB.setSalePrice(5000.0);
-        productInDB.setIsFeatured(true);
+        productInDB.setIsFeatured(false);
         repository.save(productInDB);
     }
 
     @AfterEach
-    void tearDownDB(){
+    void tearDownDB() {
         repository.deleteAll();
     }
 
     @Test
-    void testFindProductWithFilter() {
-        
+    void testFindProductWithFilter_whenGivenEmpty_shouldUseDefault() {
+        // set up
+        var emptySearchDto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty());
+
+        var expected = service.findProductWithFilter(defaultSerch);
+        // run
+        var result = service.findProductWithFilter(emptySearchDto);
+        // assert
+        assertThat(result).isEqualTo(expected);
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenPageIndex0_shouldThrow() {
+        // set up
+        var dto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(0),
+                Optional.empty());
+        // run
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.findProductWithFilter(dto);
+        });
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenPageIndexNegative_shouldThrow() {
+        // set up
+        var dto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(-1),
+                Optional.empty());
+        // run
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.findProductWithFilter(dto);
+        });
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenPageSize0_shouldThrow() {
+        // set up
+        var dto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(0), Optional.empty(),
+                Optional.empty());
+        // run
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.findProductWithFilter(dto);
+        });
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenFilterName_shouldFindOnlyProductsWhoseNameContainsSearchString() {
+        // set up
+        var searchSequence = "pro";
+        var dto = new ProductSearchDto(Optional.of(searchSequence), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty());
+        // run
+        var result = service.findProductWithFilter(dto);
+        // assert
+        assertThat(result.getContent()).allMatch((product) -> {
+            return product.getName().contains(searchSequence);
+        });
+
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenFilterFeatured_shouldFindOnlyFeaturedProducts() {
+        // set up
+        var dto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.of(true));
+        // run
+        var result = service.findProductWithFilter(dto);
+        // assert
+        assertThat(result.getContent()).allMatch((product) -> {
+            return product.getIsFeatured();
+        });
+
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenFilterPrice_shouldFindOnlyProductsWhosePriceInRange() {
+        // set up
+        var minPrice = 500.0;
+        var maxPrice = 5000.0;
+        var dto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.of(minPrice),
+                Optional.of(maxPrice), Optional.empty(), Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty());
+        // run
+        var result = service.findProductWithFilter(dto);
+        // assert
+        assertThat(result.getContent()).allMatch((product) -> {
+            return product.getSalePrice() >= minPrice && product.getSalePrice() <= maxPrice;
+        });
+
+    }
+
+    @Test
+    void testFindAllCategories_whenGivenPageSizeNegative_shouldThrow() {
+        // set up
+        var dto = new ProductSearchDto(Optional.empty(), Optional.empty(), Optional.empty(),
+                Optional.empty(), Optional.empty(), Optional.empty(), Optional.of(-1), Optional.empty(),
+                Optional.empty());
+        // run
+        assertThrows(IllegalArgumentException.class, () -> {
+            service.findProductWithFilter(dto);
+        });
     }
 
     @Test
@@ -58,7 +175,9 @@ public class ProductServiceImplTest {
         // set up
         var expected = repository.findAll().getFirst();
         // run
-        var exceptions = assertThrows(ProductNotFoundException.class, ()->{service.getProductById(expected.getId() - 1);}) ;
+        var exceptions = assertThrows(ProductNotFoundException.class, () -> {
+            service.getProductById(expected.getId() - 1);
+        });
         // assert
         assertThat(exceptions.getMessage()).isEqualTo("Product not found!");
     }
