77b2e21c17cfeaad20052587d545ca713d8b0836
hnd14 <hnd14@case.edu>
2024-06-04 10:09:15 +0700

    Set updated time to creation time on create

--- src/main/java/com/example/project/util/entities/Auditor.java
+++ src/main/java/com/example/project/util/entities/Auditor.java
@@ -38,6 +38,8 @@ public class Auditor {
     void createdAudit() {
         setCreatedTime();
         setCreatedBy();
+        setUpdatedBy();
+        setUpdatedTime();
     }
 
     void setCreatedTime() {
