ff5d49ce40d66f2ff8a20fa4788dd4dd48ceebd6
hnd14 <hnd14@case.edu>
2024-06-04 17:48:25 +0700

    Add more validations constraints

--- src/main/java/com/example/project/products/dto/Requests/PostNewProductDto.java
+++ src/main/java/com/example/project/products/dto/Requests/PostNewProductDto.java
@@ -2,9 +2,11 @@ package com.example.project.products.dto.Requests;
 
 import java.util.List;
 
+import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
 
-public record PostNewProductDto(@NotBlank String name, String desc, Long stock, Double salePrice,
-        List<Long> categoriesId, Boolean isFeatured) {
+public record PostNewProductDto(@NotBlank String name, String desc, @Min(value = 0) Long stock,
+                @Min(value = 0) Double salePrice,
+                List<Long> categoriesId, Boolean isFeatured) {
 
 }
